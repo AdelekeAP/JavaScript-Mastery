879a9aaa7081bb799233bd98dab9d3c41953e51c
AdelekeAP <aaladenusi@gmail.com>
2025-03-07 02:36:48 +0100

    progress

--- scripts/11-todo-list.js
+++ scripts/11-todo-list.js
@@ -1,4 +1,20 @@
-const todoList = [];
+const todoList = ['make dinner', 'wash dishes'];
+
+renderTodoList();
+
+function renderTodoList(){
+  let todoListHTML = '';
+  //GENERATING THE HTML
+  for(let i = 0; i < todoList.length ; i++){
+    const todo = todoList[i];
+    const html = `<p>${todo}</p>`;
+    todoListHTML += html;
+  }
+  console.log(todoListHTML);
+
+  document.querySelector('.js-todo-list').innerHTML = todoListHTML;
+} 
+
 
 function addTodo(){
   const inputElement =  document.querySelector('.js-name-input');
@@ -8,4 +24,9 @@ function addTodo(){
   console.log(todoList);
 
   inputElement.value = '';
+
+  renderTodoList();
 }
+
+
+//1st is to step data, 2nd is use data to egerante the html
